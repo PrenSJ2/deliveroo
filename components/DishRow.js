f30ad21799ecd0f8a0f636dcb402042e82d5b3a2
PrenSJ2 <seb.prentice@hotmail.com>
2022-09-18 00:58:15 +0100

    remove from basket test

--- components/DishRow.js
+++ components/DishRow.js
@@ -20,6 +20,12 @@ const DishRow = ({ id, name, description, price, image }) => {
     dispatch(addToBasket({ id, name, description, price, image }));
   };
 
+  const removeItemFromBasket = () => {
+    if (!items.length > 0) return;
+
+    dispatch(removeFromBasket({ id }));
+  };
+  
   return (
     <>
       <TouchableOpacity onPress={() => setIsPressed(!isPressed)} className={`bg-white border p-4 border-gray-200 ${isPressed && "border-b-0"}`}>
@@ -48,7 +54,7 @@ const DishRow = ({ id, name, description, price, image }) => {
       {isPressed && (
         <View className="bg-white px-4">
           <View className="flex-row items-center space-x-2 pb-3">
-            <TouchableOpacity>
+            <TouchableOpacity onPress={removeItemFromBasket}>
               <MinusCircleIcon
                 // color="items.length > 0 ? '#00CCBB' : 'gray'"
                 size={40}
